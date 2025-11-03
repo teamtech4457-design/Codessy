a92c341cfb2b5eacdc9c8abc00e3966593c2c152
Sonalijha09 <152609832+Sonalijha09@users.noreply.github.com>
2025-11-03 21:31:58 +0530

    Added complete Codessy project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-red-500 text-center">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
